ecaafed9e93167710e94edc624666473084c6a5a
William <william.gaiteiro@gmail.com>
2017-12-01 15:11:55 -0200

    Adicionado apk para download

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ require('./server/config/config');
 
 const _ = require('lodash');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const { bookRouter } = require('./server/services/book/bookRouter');
 const { shelfRouter } = require('./server/services/shelf/shelfRouter');
 
 var app = express();
-app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
 app.use('/book', bookRouter);
 app.use('/shelf', shelfRouter);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listen on port ${process.env.PORT}`);
